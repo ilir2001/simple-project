30ed779a5b2bd94262fa9d1408bda56e5eb315f2
ilir2001 <100695712+ilir2001@users.noreply.github.com>
2024-07-24 12:28:02 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/backend/public')));
 
 
 
@@ -20,7 +20,7 @@ app.get('/api/message', (req, res) => {
 
 // Catch-all handler to serve the React app
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/backend/public', 'index.html'));
+  res.sendFile(path.join(__dirname, '/public', 'index.html'));
 });
 
 
