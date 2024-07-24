a23cd86890ad232dc746483e4b338ac565926ef7
ilir2001 <100695712+ilir2001@users.noreply.github.com>
2024-07-24 11:06:55 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.get('/*', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
